8079ccf3117759cc4d16ba4c4786b5ba772d6276
= <bharaththodla.2010@rediffmail.com>
2020-11-22 12:10:05 +0000

    Maven springbot app with Jenkins pipline

---
+++ src/test/java/com/simplilearn/assessment/ImagefilphotoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagefilphotoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
